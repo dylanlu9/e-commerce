43d4d65e262b06a289203052f053a034927584c0
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-19 17:39:17 -0300

    feat(frontend navbar): added brand's logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import logo from './logo-lightblue-2642-991.png';
 import './App.css';
 
 function App() {
   return (
     <div id="app-container">
-      <nav id="navbar"></nav>
+      <nav id="navbar">
+        <img id="navbar-logo" src={logo} alt="logo"/>
+      </nav>
       <footer id="footer"></footer>
     </div>
   );
