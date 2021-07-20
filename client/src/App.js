e86234dad409d4da7255613dcd9bd4e62e00e802
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-20 15:24:12 -0300

    feat(frontend product card): added title to cards

--- client/src/App.js
+++ client/src/App.js
@@ -17,27 +17,51 @@ function App() {
         <div id="products-cards-container">
           <div className="product-card">
             <div className="product-card-image"></div>
+            <div className="product-card-title-container">
+              <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            </div>
           </div>
           <div className="product-card">
             <div className="product-card-image"></div>
+            <div className="product-card-title-container">
+              <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            </div>
           </div>
           <div className="product-card">
             <div className="product-card-image"></div>
+            <div className="product-card-title-container">
+              <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            </div>
           </div>
           <div className="product-card">
             <div className="product-card-image"></div>
+            <div className="product-card-title-container">
+              <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            </div>
           </div>
           <div className="product-card">
             <div className="product-card-image"></div>
+            <div className="product-card-title-container">
+              <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            </div>
           </div>
           <div className="product-card">
             <div className="product-card-image"></div>
+            <div className="product-card-title-container">
+              <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            </div>
           </div>
           <div className="product-card">
             <div className="product-card-image"></div>
+            <div className="product-card-title-container">
+              <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            </div>
           </div>
           <div className="product-card">
             <div className="product-card-image"></div>
+            <div className="product-card-title-container">
+              <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            </div>
           </div>
         </div>
       </div>
