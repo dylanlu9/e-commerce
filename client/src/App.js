e6160662051a63bdf42b9f92605eb8661ddb00fe
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-20 14:02:55 -0300

    feat(frontend header): added links to the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ function App() {
     <div id="app-container">
       <nav id="navbar">
         <img id="navbar-logo" src={logo} alt="logo"/>
+        <div id="navbar-links-container">
+          <a>Products</a>
+          <a>About us</a>
+          <a>Contact</a>
+        </div>
       </nav>
       <footer id="footer"></footer>
     </div>
