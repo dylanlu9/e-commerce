bbe4ff1cd6ccded112dd74cbfd873680c6337298
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-21 17:44:44 -0300

    feat(api index.js): included Sequelize library. Imported it in the main file

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const Sequelize = require('sequelize');
+
 app.get('/', (req, res) => {
    res.send("This is Dylan's E-Commerce backend.")
 });
