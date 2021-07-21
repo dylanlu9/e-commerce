1b954620f101cee3b7d11c7144d848f6a9851764
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-21 19:52:01 -0300

    feat(api sub-project): utilized Sequelize to connect to the database before starting the server

--- api/index.js
+++ api/index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const app = express();
 
 const Sequelize = require('sequelize');
+const DB_DIALECT = '-';
+const DB_USERNAME = '-';
+const DB_PASSWORD = '-';
+const DB_HOST = '-';
+const DB_PORT = 0;
+const DB_NAME = '-';
+const sequelize = new Sequelize(`${DB_DIALECT}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+   logging: (msg) => {
+      console.log(`\n${msg}\n`);
+   }
+});
 
 app.get('/', (req, res) => {
    res.send("This is Dylan's E-Commerce backend.")
 });
 
-const ourPort = 3001;
-app.listen(ourPort, () => {
-   console.log(`E-commerce backend listening at http://localhost:${ourPort}`);
-});
+sequelize.sync({
+   force: true
+})
+   .then(() => {
+      const ourPort = 3001;
+      app.listen(ourPort, () => {
+         console.log(`E-commerce backend listening at http://localhost:${ourPort}`);
+      });
+   });
