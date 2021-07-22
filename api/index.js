6d7b9cf40f7b90eca8b1bdc22916d90a86b7e0a6
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-21 22:38:08 -0300

    feat(api sub-project): defined main data models

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const DB_DIALECT = process.env.DB_DIALECT;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
@@ -14,6 +14,116 @@ const sequelize = new Sequelize(`${DB_DIALECT}://${DB_USERNAME}:${DB_PASSWORD}@$
    logging: (msg) => {
       console.log(`\n${msg}\n`);
    }
+});
+
+sequelize.define('Product', {
+   title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'No title available',
+      unique: true
+   },
+   category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'Uncategorized'
+   },
+   description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'No description available.'
+   },
+   price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 999999
+   },
+   imgUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'placeholder-img-link-here'
+   },
+   availability: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+   },
+   stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+   }
+}, {
+   tableName: 'products'
+});
+
+sequelize.define('Review', {
+   title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: '-'
+   },
+   content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: '-'
+   },
+   stars: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: 2.5
+   },
+   productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+   },
+   userId: { 
+      type: DataTypes.INTEGER,
+      allowNull: false
+   }
+}, {
+   tableName: 'reviews'
+});
+
+sequelize.define('User', {
+   email: {
+      type: DataTypes.STRING,
+      allowNull: false
+   }
+}, {
+   tableName: 'users'
+});
+
+sequelize.define('Order', {
+   status: {
+      type: DataTypes.ENUM('Created', 'Processed', 'Canceled', 'Completed'),
+      allowNull: false,
+      defaultValue: 'Created'
+   },
+   userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+   }
+}, {
+   tableName: 'orders'
+});
+
+sequelize.define('OrderLine', {
+   productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+   },
+   quantityOrdered: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+   },
+   orderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+   }
+}, {
+   tableName: 'orderlines'
 });
 
 app.get('/', (req, res) => {
