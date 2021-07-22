2ae358a790e932f5f3939c44fb26b6b041b6dc9c
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-22 17:36:41 -0300

    feat(frontend productpage component): added product description to the page

--- client/src/page-components/ProductPage/index.js
+++ client/src/page-components/ProductPage/index.js
@@ -6,6 +6,11 @@ function ProductPage () {
          <div id="product-page-main-content">
             <h1 id="product-page-product-title">Lorem ipsum dolor sit amet, consectetur.</h1>
             <div id="product-page-product-image"></div>
+            <h2 id="product-page-product-description-title">Description:</h2>
+            <p id="product-page-product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+               sed do eiusmod tempor incididunt ut labore et dolore magna
+               aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+               ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
          </div>
       </div>
    );
