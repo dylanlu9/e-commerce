45d17cc39cc9c48919fb9b2c4fcea57f2cc77a7d
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-22 17:22:36 -0300

    fix(frontend productpage component): fixed product title size and weight

--- client/src/page-components/ProductPage/index.js
+++ client/src/page-components/ProductPage/index.js
@@ -4,7 +4,7 @@ function ProductPage () {
    return (
       <div id="product-page-main-container">
          <div id="product-page-main-content">
-            <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+            <h1 id="product-page-product-title">Lorem ipsum dolor sit amet, consectetur.</h1>
             <div id="product-page-product-image"></div>
          </div>
       </div>
