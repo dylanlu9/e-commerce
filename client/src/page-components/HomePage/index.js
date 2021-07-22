24dc49fcd3a7a1a906d882470da4c275ef5d4ab7
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-22 17:01:12 -0300

    style(frontend homepage component): added missing tabs to code

--- client/src/page-components/HomePage/index.js
+++ client/src/page-components/HomePage/index.js
@@ -3,9 +3,9 @@ import './index.css';
 function HomePage () {
    return (
       <div id="app-container">
-      <div id="main-content-container">
+         <div id="main-content-container">
+         </div>
       </div>
-    </div>
    );
 }
 
