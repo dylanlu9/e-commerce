beedfe666fa471980c838e8a2ff3b598673880ae
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-22 18:01:47 -0300

    feat(frontend productpage component): added price, stock and average score placeholders

--- client/src/page-components/ProductPage/index.js
+++ client/src/page-components/ProductPage/index.js
@@ -6,11 +6,20 @@ function ProductPage () {
          <div id="product-page-main-content">
             <h1 id="product-page-product-title">Lorem ipsum dolor sit amet, consectetur.</h1>
             <div id="product-page-product-image"></div>
-            <h2 id="product-page-product-description-title">Description:</h2>
-            <p id="product-page-product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-               sed do eiusmod tempor incididunt ut labore et dolore magna
-               aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-               ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <div id="product-page-product-info-container">
+               <div id="product-page-description-container">
+                  <h2 id="product-page-product-description-title">Description:</h2>
+                  <p id="product-page-product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed do eiusmod tempor incididunt ut labore et dolore magna
+                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                     ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+               </div>
+               <div id="product-page-second-data-container">
+                  <h2 id="product-page-product-price">Price: $42</h2>
+                  <h2 id="product-page-product-stock">Stock: 9</h2>
+                  <h2 id="product-page-product-avg-score">Average score:<br></br>3/5</h2>
+               </div>
+            </div>
          </div>
       </div>
    );
