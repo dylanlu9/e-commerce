3a7832f948d7d669e464893b16eb5035d9ed9c02
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-07-26 23:58:04 -0300

    feat(frontend app): added dotenv library to the project

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -9,6 +10,7 @@ import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter, Route } from 'react-router-dom';
+dotenv.config();
 
 ReactDOM.render(
   <HashRouter>
