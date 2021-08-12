4e7e3b6b65f0441b6961f8fa5131a4132151ec24
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-08-12 19:38:23 -0300

    feat(api sub-project): added endpoint that creates a product and uploads its image to S3

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const jsonParser = require('body-parser').json();
 const app = express();
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const fileUpload = require('express-fileupload');
 
 require('dotenv').config();
+
+// Client used to upload files to S3.
+const client = new S3Client({ 
+   region: process.env.AWS_S3_REGION,
+   credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+   }
+});
 
 const sequelize_initializer = require('./src/db');
 const sequelize = sequelize_initializer();
@@ -14,6 +25,13 @@ for (const model of sequelizeModels) {
 }
 
 const clientPort = process.env.CLIENT_PORT;
+
+// 'express-fileupload' makes the uploaded files available under req.files
+// for 'multipart/form-data' requests.
+app.use(fileUpload({
+   createParentPath: true
+}));
+
 app.use((req, res, next) => {
    res.set({
       'Access-Control-Allow-Origin': `http://localhost:${clientPort}`,
@@ -21,6 +39,27 @@ app.use((req, res, next) => {
       'Access-Control-Allow-Methods': 'POST'
    });
    next();
+});
+
+app.post('/products', async (req, res) => {
+
+   // Add Product row.
+   const parsedProductData = JSON.parse(req.body.productData);
+   const ProductModel = sequelize.models.Product;
+   await ProductModel.create(parsedProductData);
+
+   // Upload image to S3.
+   const command = new PutObjectCommand({
+      Body: req.files.image.data,
+      Bucket: process.env.AWS_S3_BUCKET,
+      ContentType: req.files.image.mimetype,
+      Key: req.files.image.name,
+   });
+   await client.send(command);
+
+   // Return response.
+   res.send('Finished.');
+
 });
 
 app.get('/', (req, res) => {
