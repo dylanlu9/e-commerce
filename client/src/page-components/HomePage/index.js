a6646fe4a7d00c3d0af2d22660cd8c4ad65d40c2
Dylan Gimenez <lu9fdn@gmail.com>
2021-09-06 17:07:43 -0300

    style(homepage component): simplified formatting

--- client/src/page-components/HomePage/index.js
+++ client/src/page-components/HomePage/index.js
@@ -4,7 +4,7 @@ function HomePage () {
    return (
       <div style={style}>
          <div>
-            <ProductCrudForm></ProductCrudForm>
+            <ProductCrudForm/>
          </div>
       </div>
    );
